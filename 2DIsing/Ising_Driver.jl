733b091f68f20cc70a200cde0ede129ff688d44b
Yutan <ytzh@ucdavis.edu>
2020-08-22 16:20:40 -0700

    changes

--- 2DIsing/Ising_Driver.jl
+++ 2DIsing/Ising_Driver.jl
@@ -1,18 +1,41 @@
 include("Ising_MC.jl")
-#module Ising_Driver
 using Plots
 using BenchmarkTools
 
-#import Main.Ising_MC.spins, Main.Ising_MC.macrostate
 
 Temp = range(0.01, 10, length = 100)
 E_site = []
 S_site = []
+B = []
+χ = []
+C = []
 for T in Temp
-    mac_state = macrostate(spins(T,-1, 10^5),10000)
+    mic_state = spins(T, -1, 10^6)
+    mac_state = macrostate(mic_state, 10000)
     push!(E_site, mac_state.energy_site)
     push!(S_site, mac_state.polar_site)
+    push!(B, (0.5 * (mac_state.M4 / mac_state.M2^2 - 1)))
+    push!(
+        χ,
+        (1 / mic_state.T * (mac_state.M2 - mac_state.abs_polar^2)) / mic_state.dimension^2,
+    )
+    push!(
+        C,
+        1 / mic_state.T *
+        (mac_state.E2_site - mac_state.energy_site^2) *
+        mic_state.dimension^2,
+    )
 end
-#println("It's done.")
-plot(Temp, E_site)
-plot!(Temp, S_site)
+println("It's done.")
+plot(
+    Temp,
+    E_site,
+    label = "E_site",
+    xlabel = "T",
+    ylabel = "E_site, S_site, B, χ, C",
+    line = (1, 1.5)
+)
+plot!(Temp, S_site, label = "S_site", line = (1, 1.5))
+plot!(Temp, B, label = "B", line = (1, 1.5))
+plot!(Temp, χ, label = "χ_site", line = (1, 1.5))
+plot!(Temp, C, label = "C", line = (1, 1.5))
--- 2DIsing/Ising_MC.jl
+++ 2DIsing/Ising_MC.jl
@@ -5,14 +5,40 @@ Represent a state.
 fields:  T, J, steps, dimension, spin
 """
 mutable struct spins
-    T
-    J
+    T::Any
+    J::Any
     steps::Int64
     dimension::Int64
     spin::Array{Int64,2}
-    function spins(T, J, steps::Int64 = 10^5, dimension::Int64 = 8)
-        spin = ones(Int64, dimension, dimension)
-        new(T, J, steps, dimension, spin)
+    energy::Any
+    polar::Any
+    function spins(
+        T,
+        J,
+        steps::Int64 = 10^5,
+        dimension::Int64 = 8,
+        spin::Array{Int64,2} = ones(Int64, dimension, dimension),
+    )
+        S = spin
+        N = dimension
+        E0 = 0
+        for i = 1:N
+            for j = 1:N
+                E0 =
+                    E0 +
+                    0.5 *
+                    J *
+                    S[i, j] *
+                    (
+                        S[mod1(i - 1, N), j] +
+                        S[mod1(i + 1, N), j] +
+                        S[i, mod1(j - 1, N)] +
+                        S[i, mod1(j + 1, N)]
+                    )
+            end
+        end
+        polar = sum(spin)
+        new(T, J, steps, dimension, spin, E0, polar)
     end
 end
 
@@ -28,10 +54,20 @@ function Energy_Diff(conf::spins, index::Tuple)
     S = conf.spin
     m = index[1]
     n = index[2]
-    @assert(m<=N && m>=1 && n<=N && n>=1, "The Index of flipped spin should be within range")
-    ΔE = -conf.J * 2 * S[m, n] *
-                 (S[mod1(m - 1, N), n] + S[mod1(m + 1, N), n] +
-                  S[m, mod1(n - 1, N)] + S[m, mod1(n + 1, N)])
+    @assert(
+        m <= N && m >= 1 && n <= N && n >= 1,
+        "The Index of flipped spin should be within range"
+    )
+    ΔE =
+        -conf.J *
+        2 *
+        S[m, n] *
+        (
+            S[mod1(m - 1, N), n] +
+            S[mod1(m + 1, N), n] +
+            S[m, mod1(n - 1, N)] +
+            S[m, mod1(n + 1, N)]
+        )
     return ΔE
 end
 
@@ -45,7 +81,7 @@ Return:
 A tuple of index of the spin to flip
 """
 function flip_index(conf::spins)
-   (rand(1:conf.dimension), rand(1:conf.dimension))
+    (rand(1:conf.dimension), rand(1:conf.dimension))
 end
 
 """
@@ -59,6 +95,8 @@ Return:
 new_config::spins : the new configuration
 """
 function flip(conf::spins, index::Tuple)
+    conf.energy += Energy_Diff(conf, index)
+    conf.polar += -2 * conf.spin[index[1], index[2]]
     conf.spin[index[1], index[2]] = -1 * conf.spin[index[1], index[2]]
     return conf
 end
@@ -78,9 +116,17 @@ function Energy(conf::spins)
     E0 = 0
     for i = 1:N
         for j = 1:N
-            E0 = E0 + 0.5 * conf.J * S[i, j] *
-                     (S[mod1(i - 1, N), j] + S[mod1(i + 1, N), j] +
-                      S[i, mod1(j - 1, N)] + S[i, mod1(j + 1, N)])
+            E0 =
+                E0 +
+                0.5 *
+                conf.J *
+                S[i, j] *
+                (
+                    S[mod1(i - 1, N), j] +
+                    S[mod1(i + 1, N), j] +
+                    S[i, mod1(j - 1, N)] +
+                    S[i, mod1(j + 1, N)]
+                )
         end
     end
     return E0
@@ -109,31 +155,56 @@ function evol_cond(conf::spins, index::Tuple)
     end
 end
 
+"""
+Represent a macrostate.
+
+Fields:
+energy_site::Float64 : Energy per site for the system at a certain temperature.
+polar_site::Float64 : Polarization per site for the system at a certain temperature.
+
+"""
 struct macrostate
     energy_site::Float64
+    E2_site::Float64
     polar_site::Float64
+    abs_polar::Float64
+    M2::Float64
+    M4::Float64
+    """
+    One method of the constructor's for the macrostate type.
+
+    Parameter:
+    Conf::spins : The lattice system
+    cutoff::Int64 : Determines to how many steps would the system arrive at equilibrium(We would only calculate quantities after the cutoff step)
+    """
     function macrostate(conf::spins, cutoff::Int64)
         energy_site = 0
+        E2_site = 0
         polar_site = 0
-        E = -128
-        S = 64
-        for i in 1:conf.steps
+        abs_polar = 0
+        M2 = 0
+        M4 = 0
+        for i = 1:conf.steps
             index = flip_index(conf)
             if evol_cond(conf, index)
-                ΔE = Energy_Diff(conf, index)
-                ΔS = -2 * conf.spin[index[1], index[2]]
-                E += ΔE
-                S += ΔS
                 flip(conf, index)
             end
             i < cutoff + 1 && continue
 
-            energy_site += E / conf.dimension^2
-            polar_site += S / conf.dimension^2
+            energy_site += conf.energy / conf.dimension^2
+            E2_site += (conf.energy / conf.dimension^2)^2
+            polar_site += conf.polar / conf.dimension^2
+            abs_polar += abs(conf.polar)
+            M2 += conf.polar^2
+            M4 += conf.polar^4
         end
         energy_site = energy_site / (conf.steps - cutoff)
+        E2_site = E2_site / (conf.steps - cutoff)
         polar_site = polar_site / (conf.steps - cutoff)
+        abs_polar = abs_polar / (conf.steps - cutoff)
+        M2 = M2 / (conf.steps - cutoff)
+        M4 = M4 / (conf.steps - cutoff)
 
-        new(energy_site, polar_site)
+        new(energy_site, E2_site, polar_site, abs_polar, M2, M4)
     end
 end
