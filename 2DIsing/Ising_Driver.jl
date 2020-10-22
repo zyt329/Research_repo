edb0bf78b0aec5307f5dc5d3272772eeb785477c
Yutan <ytzh@ucdavis.edu>
2020-10-21 17:09:32 -0700

    Seperated simulation(now in driver() function) and ploting(now in graphing() function) in the driver file:Ising_Driver.jl. Resulting figures are in the folder.

--- 2DIsing/Ising_Driver.jl
+++ 2DIsing/Ising_Driver.jl
@@ -2,32 +2,6 @@ include("Ising_MC.jl")
 using Plots
 using BenchmarkTools
 
-
-Temp = range(0.1, 100, length = 100)
-E_site = []
-S_site = []
-B = []
-χ = []
-C = []
-for T in Temp
-    mic_state = spins(T, -1, 10^6)
-    mac_state = macrostate(mic_state, 10000)
-    push!(E_site, mac_state.energy_site)
-    push!(S_site, mac_state.polar_site)
-    push!(B, (0.5 * (mac_state.M4 / mac_state.M2^2 - 1)))
-    push!(
-        χ,
-        (1 / mic_state.T * (mac_state.M2 - mac_state.abs_polar^2)) / mic_state.dimension^2,
-    )
-    push!(
-        C,
-        1 / mic_state.T *
-        (mac_state.E2_site - mac_state.energy_site^2) *
-        mic_state.dimension^2,
-    )
-end
-
-
 """
 Calculate high energy expansion of E_site.
 
@@ -51,10 +25,6 @@ function Eh(T, J = 1)
     return Eh
 end
 
-Eh4(T) = Eh(T)[1]
-Eh6(T) = Eh(T)[2]
-Eh8(T) = Eh(T)[3]
-
 function EL(T, J = 1)
     EL = []
     β = 1 / T
@@ -68,25 +38,101 @@ function EL(T, J = 1)
     return EL
 end
 
-EL4(T) = EL(T)[1]
-EL6(T) = EL(T)[2]
-EL8(T) = EL(T)[3]
+function driver(dimension)
+    Temp = range(0.1, 100, length = 100)
+    E_site = []
+    S_site = []
+    B = []
+    χ = []
+    C = []
+    for T in Temp
+        mic_state = spins(T, -1, 10^6, dimension)
+        mac_state = macrostate(mic_state, 10000)
+        push!(E_site, mac_state.energy_site)
+        push!(S_site, mac_state.polar_site)
+        push!(B, (0.5 * (mac_state.M4 / mac_state.M2^2 - 1)))
+        push!(
+            χ,
+            (1 / mic_state.T * (mac_state.M2 - mac_state.abs_polar^2)) /
+            mic_state.dimension^2,
+        )
+        push!(
+            C,
+            1 / mic_state.T *
+            (mac_state.E2_site - mac_state.energy_site^2) *
+            mic_state.dimension^2,
+        )
+    end
+    return (Temp, E_site, S_site, B, χ, C, dimension)
+end
 
-println("It's done.")
-plot(Temp, E_site, label = "E_site", xlabel = "T", ylabel = "E_site", line = (1, 1.5), dpi = 500)
-plot!(Eh4, 4, 100, label = "High Temp expansion 4th order")
-plot!(Eh6, 4, 100, label = "High Temp expansion 6th order")
-plot!(Eh8, 4, 100, label = "High Temp expansion 8th order")
-plot!(EL4, 0.01, 4, label = "Low Temp expansion 4th order")
-plot!(EL6, 0.01, 4, label = "Low Temp expansion 6th order")
-plot!(EL8, 0.01, 4, label = "Low Temp expansion 8th order")
-png("E_site")
+Result = []
+push!(Result, driver(8))
+push!(Result, driver(12))
+push!(Result, driver(16))
 
-plot(Temp, S_site, label = "S_site", xlabel = "T", ylabel = "S_site", line = (1, 1.5))
-png("S_site")
-plot(Temp, B, label = "B", xlabel = "T", ylabel = "B", line = (1, 1.5))
-png("B")
-plot(Temp, χ, label = "χ_site", xlabel = "T", ylabel = "χ", line = (1, 1.5))
-png("χ")
-plot(Temp, C, label = "C", xlabel = "T", ylabel = "C", line = (1, 1.5))
-png("C")
+function graphing()
+    for i = 1:3
+        Temp, E_site, S_site, B, χ, C, dimension = Result[i][1],
+        Result[i][2],
+        Result[i][3],
+        Result[i][4],
+        Result[i][5],
+        Result[i][6],
+        Result[i][7]
+
+        Eh4(T) = Eh(T)[1]
+        Eh6(T) = Eh(T)[2]
+        Eh8(T) = Eh(T)[3]
+
+        EL4(T) = EL(T)[1]
+        EL6(T) = EL(T)[2]
+        EL8(T) = EL(T)[3]
+
+        println("It's done.")
+        E_site_plt = plot(
+            Temp,
+            E_site,
+            label = "E_site",
+            xlabel = "T",
+            ylabel = "E_site",
+            line = (1, 1.5),
+            dpi = 800,
+        )
+        plot!(Eh4, 4, 100, label = "High Temp expansion 4th order", linestyle = :dash)
+        plot!(Eh6, 4, 100, label = "High Temp expansion 6th order", linestyle = :dash)
+        plot!(Eh8, 4, 100, label = "High Temp expansion 8th order", linestyle = :dash)
+        plot!(EL4, 0.01, 4, label = "Low Temp expansion 4th order", linestyle = :dash)
+        plot!(EL6, 0.01, 4, label = "Low Temp expansion 6th order", linestyle = :dash)
+        plot!(EL8, 0.01, 4, label = "Low Temp expansion 8th order", linestyle = :dash)
+        png("E_site_$dimension")
+
+        plot(
+            Temp,
+            S_site,
+            label = "S_site",
+            xlabel = "T",
+            ylabel = "S_site",
+            line = (1, 1.5),
+            dpi = 800,
+        )
+        png("S_site_$dimension")
+        plot(Temp, B, label = "B", xlabel = "T", ylabel = "B", line = (1, 1.5), dpi = 800)
+        png("B_$dimension")
+        plot(
+            Temp,
+            χ,
+            label = "χ_site",
+            xlabel = "T",
+            ylabel = "χ",
+            line = (1, 1.5),
+            dpi = 800,
+        )
+        png("χ_$dimension")
+        plot(Temp, C, label = "C", xlabel = "T", ylabel = "C", line = (1, 1.5), dpi = 800)
+        png("C_$dimension")
+
+    end
+end
+
+graphing()
